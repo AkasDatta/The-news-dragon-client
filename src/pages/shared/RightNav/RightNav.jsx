9d7eb861ebca23c401e862ed4b88301e0d964bf9
Akas_Datta <012akasdatta@gmail.com>
2023-04-30 21:48:49 +0600

    added qzone component and improve rightnav

--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png';
 
 const RightNav = () => {
     return (
         <div>
-            <h4>login With</h4>
-            <Button variant="outline-primary"><FaGoogle /> Login with Google</Button>
-            <Button variant="outline-secondary"><FaGithub />Login With Github</Button>
+            <h4 className='mb-4'>login With</h4>
             <div>
-                <h4>Find Us On</h4>
-                <ListGroup >
+                <Button variant="outline-primary"><FaGoogle /> Login with Google</Button>
+                <Button variant="outline-secondary"><FaGithub />Login With Github</Button>
+            </div>
+            <div>
+                <h4 className='my-4'>Find Us On</h4>
+                <ListGroup>
                     <a href="https://www.facebook.com/iamakasdatta/" className='text-decoration-none'>
                         <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item></a>
                     <a href="https://twitter.com/DattaAkas" className='text-decoration-none'>
@@ -20,6 +24,12 @@ const RightNav = () => {
                         <ListGroup.Item><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
                     </a>
                 </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div style={{backgroundImage: `url(${bg})`, color: 'white'}} className='text-center p-5'>
+                <h2>Create an <br /> Amazing <br /> Newspaper</h2>
+                <p>Discover thousands of <br /> options, easy to <br /> customize layouts, one- <br /> click to import demo <br /> and much more.</p>
+                <Button variant="danger" className='p-3 px-4 rounded-0'>Learn More</Button>
             </div>
         </div>
     );
