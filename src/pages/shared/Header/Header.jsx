a686e4f35d87d443b0278cbd342d1554aaa36b4a
Akas_Datta <012akasdatta@gmail.com>
2023-05-01 20:29:13 +0600

    added provider component and modified others

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -3,6 +3,7 @@ import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -25,7 +26,9 @@ const Header = () => {
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
                             <Nav className="mx-auto my-2 my-lg-0">
-                                <Nav.Link href="#action1">Home</Nav.Link>
+                                <Nav.Link href="#action1">
+                                    <Link to="/">Home</Link>
+                                </Nav.Link>
                                 <Nav.Link href="#action2">About</Nav.Link>
                                 <Nav.Link href="#action3">Career</Nav.Link>
                             </Nav>
