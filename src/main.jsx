6cbb38fdbce1fd3aa2de97a892f3a53950794f29
Akas_Datta <012akasdatta@gmail.com>
2023-05-07 10:28:08 +0600

    modified some file and folder and add helmetprovider

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Routes.jsx';
 import AuthProvider from './providers/AuthProvider';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <Helmet>
+          <title>Dragon News</title>
+          <link rel="icon" type="image/svg+xml" href="/public/icon.jpg" />
+        </Helmet>
+          <RouterProvider router={router}></RouterProvider>
+       
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
